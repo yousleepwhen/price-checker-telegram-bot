2903902f07b8a59539454d821c896c52b053aa0e
forsyphilis <forsyphilis@2tis.com>
2017-08-16 16:30:18 +0900

    phantomjs npm 설치 config

--- bot.js
+++ bot.js
@@ -37,7 +37,7 @@ var options = {
         width: 1150
         , height: 800
     },
-    phantomPath:'/opt/phantomjs/bin/phantomjs',
+    // phantomPath:'/opt/phantomjs/bin/phantomjs',
     userAgent: 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_2 like Mac OS X; en-us)'
     + ' AppleWebKit/531.21.20 (KHTML, like Gecko) Mobile/7B298g'
 };
